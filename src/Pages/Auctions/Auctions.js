6bcc52fb1e145bbc9b974f9811e0dadc1ce930de
Rabiaparlak <68352029+Rabiaparlak@users.noreply.github.com>
2023-06-10 08:04:41 +0300

    Single Commit
    Project is completed.

---
+++ src/Pages/Auctions/Auctions.js
@@ -0,0 +1,9 @@
+import styles from "./Auctions.scss";
+
+const Auctions = () => {
+  return(
+    <div>xd</div>
+  )
+}
+
+export {Auctions};
---
+++ src/Pages/Home/Home.js
@@ -0,0 +1,12 @@
+import styles from "./Home.scss";
+
+const Home = () => {
+  return (
+    <div className={styles.wrapper}>
+      <h1>MARKETPLACE APP</h1>
+      <h3>NFT</h3>
+    </div>
+  );
+};
+
+export { Home };
